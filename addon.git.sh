679ff9d354c258e2e7f3535076e96b075205814f
Alyssa Nolan <canalyssa@outlook.com>
2019-01-20 22:37:31 +0530

    Added support for subdir

--- addon.git.sh
+++ addon.git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if [ -d "./.git" ]; then
+if git rev-parse --git-dir > /dev/null 2>&1; then
   branch=`git branch | grep \\* | cut -d ' ' -f2`;
   export PS1="$PS1_ORIGINAL($branch) $ ";
 else
